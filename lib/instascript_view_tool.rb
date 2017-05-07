060a53a912a3892639e865e2bb6ab3366ec1f9c1
Anna <a.dimitrova.au@gmail.com>
2017-05-07 17:15:08 +1000

    Insta tool initial commit

---
+++ lib/instascript_view_tool.rb
@@ -0,0 +1,6 @@
+require "instascript_view_tool/version"
+require "instascript_view_tool/renderer"
+
+module InstascriptViewTool
+  # Your code goes here...
+end
---
+++ lib/instascript_view_tool/version.rb
@@ -0,0 +1,3 @@
+module InstascriptViewTool
+  VERSION = "0.1.0"
+end
